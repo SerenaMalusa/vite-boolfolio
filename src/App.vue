f2a8caecf01eb5ea501f2ad1bbec7502684f0fbe
SerenaMalusa <sere.malu@gmail.com>
2024-04-12 19:27:35 +0200

    feat: receive the data trough axios call

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Hello world!",
+      tech_badges: [],
+    };
+  },
+
+  methods: {
+    fecthProjects() {
+      axios.get(`http://127.0.0.1:8000/api/projects`).then((res) => {
+        console.log(res.data.data);
+        this.tech_badges = res.data.data[0].technologies_badges;
+      });
+    },
+  },
+
+  mounted() {
+    this.fecthProjects();
+  },
+};
 </script>
 
 <template>
   <div class="container">
-    <h1>Hello world</h1>
+    <h1>{{ title }}</h1>
+    <div></div>
   </div>
 </template>
 
